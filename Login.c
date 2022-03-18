f09950767ac2ed239ac3e1fc4ee56583822093e9
cristian <camilorestrepo@outlook.es>
2022-03-18 13:52:20 -0600

    modificado archivo.c y creado login.c

---
+++ Login.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    printf("Me estoy logeando en mi pagina web en c");
+    return 0;
+}
--- archivo.c
+++ archivo.c
@@ -5,5 +5,6 @@ int main(){
     int resultado = 8+9;
     resultado = resultado -2;
     printf("El resultado de la suma es: %d\n",resultado);
+    printf("Ese es el resultado mijin");
     return 0;
 }
